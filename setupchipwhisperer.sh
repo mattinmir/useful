96be10b12bd3c75e2d12f842ff770e524f90a680
mattinmir <mattinmir@users.noreply.github.com>
2016-07-22 15:28:56 +0100

    Create setupchipwhisperer.sh

---
+++ setupchipwhisperer.sh
@@ -0,0 +1,15 @@
+sudo su
+
+# Update repos
+cd ~/chipwhisperer
+git pull
+cd openadc
+git pull
+
+# Terminal Styling
+echo "PS1='\[\e[0;32m\]\u\[\e[m\] \[\e[1;34m\]\w\[\e[m\] \[\e[1;32m\]\$\[\e[m\]$
+
+# Set keyboard to uk
+setxkbmap gb
+
+
