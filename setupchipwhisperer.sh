c46cba3e33d2a1633bcc9ee9b3ea8367ffe8ae82
mattinmir <mattinmir@users.noreply.github.com>
2016-07-25 07:14:47 -0300

    Update setupchipwhisperer.sh

--- setupchipwhisperer.sh
+++ setupchipwhisperer.sh
@@ -5,7 +5,7 @@ cd openadc
 git pull
 
 # Terminal Styling
-echo "PS1='\[\e[0;32m\]\u\[\e[m\] \[\e[1;34m\]\w\[\e[m\] \[\e[1;32m\]\$\[\e[m\]$" >> ~/.bashrc 
+echo PS1='\[\e[0;32m\]\u\[\e[m\] \[\e[1;34m\]\w\[\e[m\] \[\e[1;32m\]\$\[\e[m\] ' >> ~/.bashrc 
 
 # Set keyboard to uk
 setxkbmap gb
